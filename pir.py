4393aee9211de3a0da1b1fa5b0534dd0a70c6f14
edwinvarroyo <e28v11a96@gmail.com>
2015-11-22 01:38:14 -0600

    jhfv<zxjchvc

--- pir.py
+++ pir.py
@@ -26,4 +26,5 @@ while True:
     if current_state != previous_state:
     	contador=contador+1;
         new_state = "HIGH" if current_state else "LOW"
-        print("movimiento " contador)
+        print("movimiento ")
+        print(contador)
