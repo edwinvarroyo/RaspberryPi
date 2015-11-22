1810fab321d8d23264b14ea5fcefebebbc8d7522
edwinvarroyo <e28v11a96@gmail.com>
2015-11-22 01:07:53 -0600

    SDF

--- pir.py
+++ pir.py
@@ -1,22 +1,18 @@
-import RPi.GPIO as GPIO    #Importamos la libreria GPIO
-import time                #Importamos time
-from time import gmtime, strftime  #importamos gmtime y strftime
-GPIO.setmode(GPIO.BCM)             #Configuramos los pines GPIO como BCM
-PIR_PIN = 21                        #Usaremos el pin GPIO nº7
-GPIO.setup(PIR_PIN, GPIO.IN)       #Lo configuramos como entrada
- 
-GPIO.setup(20, GPIO.OUT)          #Configuramos el pin 17 como salida (para un led)
- 
-try:
-    while True:  #Iniciamos un bucle infinito
-        if GPIO.input(PIR_PIN):  #Si hay señal en el pin GPIO nº7
-           GPIO.output(20,True) #Encendemos el led
-            time.sleep(1)        #Pausa de 1 segundo
-            timex = strftime("%d-%m-%Y %H:%M:%S", gmtime()) #Creamos una cadena de texto con la hora
-            print timex + " MOVIMIENTO DETECTADO"  #La sacamos por pantalla
-            time.sleep(1)  #Pausa de 1 segundo
-	        GPIO.output(20,False)  #Apagamos el led
-        time.sleep(1)              #Pausa de 1 segundo y vuelta a empezar
-except KeyboardInterrupt:   #Si el usuario pulsa CONTROL + C...
-    print "quit"            #Anunciamos que finalizamos el script
-    GPIO.cleanup()    
+import RPi.GPIO as GPIO
+import time
+
+sensor = 4
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(sensor, GPIO.IN, GPIO.PUD_DOWN)
+
+previous_state = False
+current_state = False
+
+while True:
+    time.sleep(0.1)
+    previous_state = current_state
+    current_state = GPIO.input(sensor)
+    if current_state != previous_state:
+        new_state = "HIGH" if current_state else "LOW"
+        print("GPIO pin %s is %s" % (sensor, new_state))
